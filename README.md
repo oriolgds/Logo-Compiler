04016346441936ddb6352133d419059cb4072033
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-04-12 15:56:10 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Logo Compiler
- A logo interpreter to do codelearn exercices
+Un interprete logo para realizar los ejercicios de la codeleathon sin ningun esfuerzo
+
+### Como quitar la etiqueta nocopy de Codelearn para poder copiar los ejercicios
+#### Pega esto en tu terminal del navegador
+```javascript
+document.querySelectorAll("nocopy").forEach(node => {
+  node.innerHTML = " ";
+})
+```
---
+++ web/index.js
@@ -0,0 +1,24 @@
+const textarea = document.getElementById("code-input");
+const btnExecute = document.getElementById("btn-execute");
+const btnStop = document.getElementById("btn-stop");
+const btnRestart = document.getElementById("btn-restart");
+const console = document.getElementById("console");
+const consoleInput = document.getElementById("console-input");
+function execute() {
+    eel.compile(textarea.value);
+}
+function stop(){    
+    eel.stop();
+    window.close();
+    console.log("Stop!");
+}
+btnExecute.addEventListener('click', execute);
+btnStop.addEventListener('click', stop);
+eel.expose(writeOnConsole);
+function writeOnConsole(textToWrite = "", style="normal"){
+    console.innerHTML += `<span class="${style}">${textToWrite}</span><br>`;
+}
+textarea.addEventListener('keyup', ()=>{
+    localStorage.setItem('code-text', textarea.value);
+});
+textarea.value = localStorage.getItem("code-text");
